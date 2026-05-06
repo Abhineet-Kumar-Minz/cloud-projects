a8dc34a74e757b4df7bdcdd011140cef36f322a2
Abhineet-Kumar-Minz <www.akm2003@gmail.com>
2026-05-07 01:01:34 +0530

    Fix API URL for production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ 
+  baseURL: window.location.hostname === 'localhost' 
+    ? 'http://localhost:5000/api'
+    : '/api'
+});
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
